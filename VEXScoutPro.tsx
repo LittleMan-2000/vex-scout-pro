0d06bbd16b3471b0e7ea4e0b287aadf52854bbe9
LittleMan-2000 <hippmann_ch@student.kings.edu.au>
2025-08-22 16:52:47 +1000

    Add files via upload

---
+++ VEXScoutPro.tsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, PieChart, Pie, Cell } from 'recharts';
+import { 
+  Users, TrendingUp, Plus, Star, LogIn, LogOut, Settings, Search, 
+  Trophy, Target, Shield, CheckCircle, UserPlus, Share2, Bell, 
+  Brain, Loader, MapPin, Clock, Award, Gamepad, AlertCircle,
+  RefreshCw, ExternalLink, Calendar, Eye, Filter, Download
+} from 'lucide-react';
